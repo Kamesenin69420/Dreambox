5ae4e30e4818d52afef1e6dcb042cf114dfcbd29
Kamesenin69420 <sogeking95@live.fr>
2025-03-28 18:17:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const animeList = [
   { title: "One Piece", episodes: 1000, watched: 150 },
   { title: "Naruto", episodes: 700, watched: 500 }, 
-  { title: "Highschool of the dead", episodes: 12, watched: 4 },
+  { title: "Highschool of the Dead",
+    episodes: [
+      { title: "Episode 1", watched: false },
+      { title: "Episode 2", watched: false },
+      { title: "Episode 3", watched: false },
+      { title: "Episode 4", watched: false },
+      { title: "Episode 5", watched: false },
+      { title: "Episode 6", watched: false },
+      { title: "Episode 7", watched: false },
+      { title: "Episode 8", watched: false },
+      { title: "Episode 9", watched: false },
+      { title: "Episode 10", watched: false },
+      { title: "Episode 11", watched: false },
+      { title: "Episode 12", watched: false },
+    ],
+    image: "highschool-of-the-dead.jpg"},
 ];
 
 function displayAnimes() {
