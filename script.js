407a1243a4348d219a97023aa17d5261fadcc25c
Kamesenin69420 <sogeking95@live.fr>
2025-03-29 00:26:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,13 @@ const animeList = [
     image: "https://i.imgur.com/G8H1ahP.jpeg"
   },
   {
+    title: "Jimihen!! Jimiko wo Kaechau Jun Isei Kouyuu",
+    episodes: 5,
+    watched: 0,
+    image: "  https://www.nautiljon.com/images/anime/00/15/mini/jimihen_jimiko_wo_kae_chau_jun_isei_kouyuu_9851.webp
+"
+  },
+  {
     title: "Highschool of the Dead",
     episodes: 12,
     watched: 5,
