72793d7d08df8a06c40006c306f75ae648aefa01
Kamesenin69420 <sogeking95@live.fr>
2025-03-28 18:53:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 const animeList = [
-  { title: "One Piece", episodes: 1000, watched: 150 },
-  { title: "Naruto", episodes: 700, watched: 500 }, 
-  { title: "Highschool of the Dead", episodes: 12, watched: 5 },
+  {
+    title: "One Piece",
+    episodes: 1000,
+    watched: 150,
+    image: "one-piece.jpg"
+  },
+  {
+    title: "Naruto",
+    episodes: 700,
+    watched: 500,
+    image: "naruto.jpg"
+  },
+  {
+    title: "Highschool of the Dead",
+    episodes: 12,
+    watched: 5,
+    image: "highschool_of_the_dead_main.jpg"
+  }
 ];
+
 function displayAnimes() {
   const animeContainer = document.getElementById('anime-list');
+  animeContainer.innerHTML = ""; // On vide avant d'ajouter les animés
+
   animeList.forEach(anime => {
     const animeDiv = document.createElement('div');
     animeDiv.classList.add('anime-item');
     animeDiv.innerHTML = `
+      <img src="${anime.image}" alt="${anime.title}" class="anime-image">
       <h3>${anime.title}</h3>
       <p>Episodes: ${anime.episodes}</p>
       <p>Vu: ${anime.watched}</p>
