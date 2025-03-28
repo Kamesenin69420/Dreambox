d92136d75888d3aa31fdf050ec2da11f6e76411a
Kamesenin69420 <sogeking95@live.fr>
2025-03-28 17:37:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const animeList = [
+  { title: "One Piece", episodes: 1000, watched: 150 },
+  { title: "Naruto", episodes: 700, watched: 500 },
+];
+
+function displayAnimes() {
+  const animeContainer = document.getElementById('anime-list');
+  animeList.forEach(anime => {
+    const animeDiv = document.createElement('div');
+    animeDiv.classList.add('anime-item');
+    animeDiv.innerHTML = `
+      <h3>${anime.title}</h3>
+      <p>Episodes: ${anime.episodes}</p>
+      <p>Vu: ${anime.watched}</p>
+    `;
+    animeContainer.appendChild(animeDiv);
+  });
+}
+
+displayAnimes();
